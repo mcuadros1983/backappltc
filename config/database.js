927ed58c386366f223be3c279b9ced1450bad92e
Miguel Cuadros <cuadros_miguel@hotmail.com>
2024-05-05 19:13:49 -0300

    se añaden valores de base

--- config/database.js
+++ config/database.js
@@ -13,10 +13,10 @@ const {
   DB_DIALECT,
 } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: DB_DIALECT,
+const sequelize = new Sequelize("railway", "postgres", "SREHCLpjnkmHsHuwyqiZGugPMlwmCVwO", {
+  host: "roundhouse.proxy.rlwy.net",
+  port: 14155,
+  dialect: "postgres",
   
   // Puedes agregar más configuraciones aquí según sea necesario
 });
