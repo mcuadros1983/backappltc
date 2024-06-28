659a82bd7095d17ccad660366b11b5e35166a41a
Miguel Cuadros <cuadros_miguel@hotmail.com>
2024-06-28 18:07:44 -0300

    modifica ruta de creacion clientes con /

--- routes/gmedias/clientesRoute.js
+++ routes/gmedias/clientesRoute.js
@@ -7,14 +7,14 @@ import { authenticate } from '../../middleware/authMiddleware.js';
 const clientesRouter = Router()
 
 // Rutas para clientes
-clientesRouter.get('/clientes', clientesController.obtenerClientes);
-clientesRouter.get('/clientes/:clienteId', clientesController.obtenerClientePorId);
-clientesRouter.post('/clientes-new', clientesController.crearCliente);
-clientesRouter.put('/clientes/:clienteId', clientesController.actualizarCliente);
-clientesRouter.delete('/clientes/:clienteId', clientesController.eliminarCliente);
+clientesRouter.get('/clientes/', clientesController.obtenerClientes);
+clientesRouter.get('/clientes/:clienteId/', clientesController.obtenerClientePorId);
+clientesRouter.post('/clientes-new/', clientesController.crearCliente);
+clientesRouter.put('/clientes/:clienteId/', clientesController.actualizarCliente);
+clientesRouter.delete('/clientes/:clienteId/', clientesController.eliminarCliente);
 
 // Rutas adicionales para obtener información relacionada
-clientesRouter.get('/clientes/:clienteId/ventas', clientesController.obtenerVentasDeCliente);
-clientesRouter.get('/clientes/:clienteId/cuenta-corriente', clientesController.obtenerCuentaCorrienteDeCliente);
+clientesRouter.get('/clientes/:clienteId/ventas/', clientesController.obtenerVentasDeCliente);
+clientesRouter.get('/clientes/:clienteId/cuenta-corriente/', clientesController.obtenerCuentaCorrienteDeCliente);
 
 export default clientesRouter;
