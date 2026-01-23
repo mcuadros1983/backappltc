b141eed67611f04e70518dcc7ded9e22aeb3b755
Miguel <mcuadros83@gmail.com>
2026-01-23 18:08:39 -0300

    se modifica modelo cupon

--- models/caja/cuponModel.js
+++ models/caja/cuponModel.js
@@ -54,7 +54,8 @@ const Cupon = sequelize.define(
     // },
     venta_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: null,
     },
   },
   {
